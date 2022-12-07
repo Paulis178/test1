882ae016f552bd66c785c376e74638f09faacd19
Pavel <96879553+Paulis178@users.noreply.github.com>
2022-12-07 23:48:49 +0300

    gg
    gg

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(window).on('load', function() {
+  $('#header').vide('./video/cover.mp4', {
+    bgColor: '#7b8287'
+  })
+})
---
+++ marketing/js/main.js
@@ -0,0 +1,33 @@
+$(function(){
+
+  $(".menu a, .go-top").on("click", function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+  $('.slider-blog__inner').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $('.menu__btn, .menu a').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  var mixer = mixitup('.portfolio__content');
+
+});
